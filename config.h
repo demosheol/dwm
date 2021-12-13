7733f9b449816c78ca587d6335c51061c3161f9e
demosheol <94229708+demosheol@users.noreply.github.com>
2021-12-13 12:44:27 -0300

    Add files via upload

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12" };
-static const char dmenufont[]       = "monospace:size=11";
+static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#151515"; /* #1b1b1b */
 static const char col_gray2[]       = "#505050"; /* #fb9fb1 */
 static const char col_gray3[]       = "#d0d0d0"; /* #d0d0d0 or #fb9fb1 */
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
