c339dabb4b876dac3cc44c4c2a8af5c5915255c3
Arthur Soares <94229708+demosheol@users.noreply.github.com>
2022-05-14 17:52:29 +0000

    dark theme

--- config.h
+++ config.h
@@ -2,18 +2,23 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 12;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Iosevka:size=10" };
-static const char dmenufont[]       = "Iosevka:size=10";
-static const char col_gray1[]       = "#000000"; /* #222222 */
+static const char *fonts[]          = { "Gohufont:size=10" };
+static const char dmenufont[]       = "Gohufont:size=10";
+/* Background color */
+static const char col_gray1[]       = "#000000";
+/* Inactive window border color */
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee"; /* #eeeeee */
-static const char col_cyan[]        = "#5A5A5A"; /* #005577 or #404040 */
+/* Font color */
+static const char col_gray3[]       = "#ffffff";
+/* Current tag and current window font color*/
+static const char col_gray4[]       = "#eeeeee";
+/* Top bar second color and active window border color */
+static const char col_cyan[]        = "#202020";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -63,13 +68,19 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *browser[] = { "firefox", NULL };
+static const char *redshift[] = { "redshiftgui", NULL };
+static const char *keepass[]  = { "keepassxc", NULL };
+static const char *gnuemacs[] = { "emacs", NULL };
+static const char *browser[]  = { "firefox", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function          argument */
 	{ MODKEY,                       XK_d,      spawn,            {.v = dmenucmd } },
-	{ MODKEY,	                XK_Return, spawn,            {.v = termcmd } },
-	{ MODKEY,			XK_w,	   spawn,	     {.v = browser } },
+	{ MODKEY,	                    XK_Return, spawn,            {.v = termcmd } },
+	{ MODKEY,			            XK_w,	   spawn,	         {.v = browser } },
+	{ MODKEY,           			XK_r,	   spawn,	         {.v = redshift } },
+	{ MODKEY|ShiftMask,		        XK_k,	   spawn,  	         {.v = keepass } },
+    { MODKEY,                       XK_e,      spawn,            {.v = gnuemacs } },
 	{ MODKEY,                       XK_t,      togglebar,        {0} },
 	{ MODKEY,                       XK_j,      focusstack,       {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,       {.i = -1 } },
@@ -79,17 +90,17 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,         {.f = +0.05} },
 	{ MODKEY,                       XK_z,      zoom,             {0} },
 	{ MODKEY,                       XK_Tab,    view,             {0} },
-	{ MODKEY,	                XK_q,      killclient,       {0} },
+	{ MODKEY,	                    XK_q,      killclient,       {0} },
 	{ MODKEY,                       XK_t,      setlayout,        {.v = &layouts[0]} },
 	{ MODKEY,                       XK_y,      setlayout,        {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,        {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating,   {0} },
-	{ MODKEY,			XK_f,	   togglefullscreen, {0} },
+	{ MODKEY,		            	XK_f,	   togglefullscreen, {0} },
 	{ MODKEY,                       XK_0,      view,             {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,              {.ui = ~0 } },
 	{ MODKEY,                       XK_Left,   focusmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_Left,   focusmon,         {.i = +1 } },
-	{ MODKEY, 	                XK_Right,  tagmon,           {.i = -1 } },
+	{ MODKEY, 	                    XK_Right,  tagmon,           {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_Right,  tagmon,           {.i = +1 } },
 	{ MODKEY,                       XK_minus,  setgaps,          {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,          {.i = +1 } },
